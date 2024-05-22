8bfcc8d79a9bf8fd4463227a74ff1dfc8e4ec748
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-05-22 22:48:57 +0300

    Доработка UI (доработана таблица, добавлены кнопки)

--- src/main/java/com/paidservices/paidservicesdesktopapp/PaidServiceApplication.java
+++ src/main/java/com/paidservices/paidservicesdesktopapp/PaidServiceApplication.java
@@ -12,7 +12,7 @@ public class PaidServiceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PaidServiceApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1000);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1020);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
 
         stage.setTitle("АИС MedClinic");
--- src/main/java/com/paidservices/paidservicesdesktopapp/visitation/controller/VisitationController.java
+++ src/main/java/com/paidservices/paidservicesdesktopapp/visitation/controller/VisitationController.java
@@ -1,8 +1,25 @@
 package com.paidservices.paidservicesdesktopapp.visitation.controller;
 
 import com.paidservices.paidservicesdesktopapp.visitation.model.Visitation;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
 public class VisitationController {
     public TableView<Visitation> visitationTable;
+    public Button addVisitationButton;
+    public Button editVisitationButton;
+    public Button deleteVisitationButton;
+
+    public void addVisitationAction(ActionEvent actionEvent) {
+
+    }
+
+    public void editVisitationAction(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteVisitationAction(ActionEvent actionEvent) {
+
+    }
 }
