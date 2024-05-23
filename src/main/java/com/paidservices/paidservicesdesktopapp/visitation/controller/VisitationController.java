bd2b233692ac0f16aae0313da019b849b803c9e9
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-05-23 18:00:57 +0300

    Реализовано получение информации о посещениях с сервиса

--- src/main/java/com/paidservices/paidservicesdesktopapp/visitation/controller/VisitationController.java
+++ src/main/java/com/paidservices/paidservicesdesktopapp/visitation/controller/VisitationController.java
@@ -1,15 +1,43 @@
 package com.paidservices.paidservicesdesktopapp.visitation.controller;
 
 import com.paidservices.paidservicesdesktopapp.visitation.model.Visitation;
+import com.paidservices.paidservicesdesktopapp.webclient.client.WebClient;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import org.controlsfx.control.Notifications;
 
 public class VisitationController {
+    private final WebClient client = WebClient.getInstance();
+
     public TableView<Visitation> visitationTable;
     public Button addVisitationButton;
     public Button editVisitationButton;
     public Button deleteVisitationButton;
+
+    @FXML
+    public void initialize() {
+        client.getVisitations()
+                .thenAccept(visitations -> {
+                    Platform.runLater(() -> {
+                        visitationTable.setItems(FXCollections.observableArrayList(visitations));
+                    });
+                })
+                .exceptionally(throwable -> {
+                    Platform.runLater(() -> {
+                        Notifications
+                                .create()
+                                .title("Ошибка!")
+                                .text("Не удалось загрузить данные о посещениях!")
+                                .showError();
+                    });
+
+                    throw new RuntimeException(throwable);
+                });
+    }
 
     public void addVisitationAction(ActionEvent actionEvent) {
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,14 @@ module com.paidservices.paidservicesdesktopapp {
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
 
+    requires retrofit2;
+    requires retrofit2.converter.jackson;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+
     opens com.paidservices.paidservicesdesktopapp to javafx.fxml;
     opens com.paidservices.paidservicesdesktopapp.visitation.controller to javafx.fxml;
+    opens com.paidservices.paidservicesdesktopapp.visitation.model to com.fasterxml.jackson.databind,
+            com.fasterxml.jackson.datatype.jsr310;
     exports com.paidservices.paidservicesdesktopapp;
 }
