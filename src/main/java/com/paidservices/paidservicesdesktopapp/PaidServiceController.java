9d1b0da9f10d883856df3d8a54bce15580e5be81
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-05-26 22:03:12 +0300

    Изменён порядок открытия модулей: модули открываются по двойному клику на главной вкладке

--- src/main/java/com/paidservices/paidservicesdesktopapp/PaidServiceController.java
+++ src/main/java/com/paidservices/paidservicesdesktopapp/PaidServiceController.java
@@ -1,7 +1,43 @@
 package com.paidservices.paidservicesdesktopapp;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class PaidServiceController {
     public TabPane modulePane;
+
+    public void visitationModuleOnClick(MouseEvent mouseEvent) throws IOException {
+        if (mouseEvent.getClickCount() < 2)
+            return;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(PaidServiceApplication.class.getResource("visitation/visitation-view.fxml"));
+        Node rootNode = fxmlLoader.load();
+
+        Tab visitationTab = new Tab("Модуль посещений");
+        visitationTab.setStyle("-fx-font-size: 18; -fx-padding: 10");
+        visitationTab.setContent(rootNode);
+
+        modulePane.getTabs().add(visitationTab);
+        modulePane.getSelectionModel().select(visitationTab);
+    }
+
+    public void financialModuleOnClick(MouseEvent mouseEvent) throws IOException {
+        if (mouseEvent.getClickCount() < 2)
+            return;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(PaidServiceApplication.class.getResource("financial/financial-view.fxml"));
+        Node rootNode = fxmlLoader.load();
+
+        Tab visitationTab = new Tab("Модуль фин.отчётности");
+        visitationTab.setStyle("-fx-font-size: 18; -fx-padding: 10");
+        visitationTab.setContent(rootNode);
+
+        modulePane.getTabs().add(visitationTab);
+        modulePane.getSelectionModel().select(visitationTab);
+    }
 }
